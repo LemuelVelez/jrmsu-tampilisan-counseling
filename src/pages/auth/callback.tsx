0b15a27ae2d43fc513154e46c160901d72cd50aa
lemuel <pluklem07@gmail.com>
2025-11-22 17:18:35 +0800

    feat: add initial page structures for 404, auth, and dashboard views

---
+++ src/pages/auth/callback.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const callback = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default callback
---
+++ src/pages/auth/verify-email.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const verifyemail = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default verifyemail
